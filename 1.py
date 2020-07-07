fa0e4e90f1489bc18da43133a1ee437b92302a44
atna26 <58582990+atna26@users.noreply.github.com>
2020-07-07 20:53:48 +0700

    Add files via upload

--- 1.py
+++ 1.py
@@ -12,27 +12,15 @@ import base64
 app = Flask(__name__, static_url_path='/static/')
 @app.route('/', methods = ['GET', 'POST'])
 def handle_request():
-    '''
-    imagefile = flask.request.files['image']
-    filename = werkzeug.utils.secure_filename(imagefile.filename)
-    print("\nReceived image File name : " + imagefile.filename)
-    imagefile.save(filename)
-    '''
+
     imageresult = omr2("D:/Data/kuliah/TA-doc/Code/FinalCopy/androidFlask.jpg")
     path = r'D:\Data\kuliah\TA-doc\Code\FinalCopy\static'
     data_uri = base64.b64encode(open('detected.png', 'rb').read()).decode('utf-8')
     img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
 
-    '''
-    return rv
-    
-    imageresult = omr2("D:/Data/kuliah/TA-doc/Code/FinalCopy/androidFlask.jpg")
-    imageresult.save("static/detected.png", "PNG")
-    with open(static,"rb") as imageresult:
-        encoded_string=base64.b64encode(imageresult.read())
-    '''
+
     return flask.jsonify(data_uri)
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
